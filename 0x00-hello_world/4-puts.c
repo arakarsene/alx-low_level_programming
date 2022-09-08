92c98121d4487d96208c9095f4dbafa5378228e7
arakarsene <arakarsebe@gmail.com>
2022-09-08 11:56:21 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+int main(void)
+
+{
+
+	    puts("\"Programming is like building a multilingual puzzle");
+
+	        return 0;
+
+}
+
+
