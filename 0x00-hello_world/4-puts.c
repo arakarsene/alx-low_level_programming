0039be68727051e114f03e0e370ab7e8f509bb0e
arakarsene <arakarsebe@gmail.com>
2022-09-08 12:41:09 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,17 @@
+
 #include <stdio.h>
 /**
  *
- * *main - pout point
+ * *main - Entry point
  *
  * *
  *
- * *return : 0( success)
+ * *return :Always 0 (Succes)
  *
  * */
 int main(void)
-{
 
+{
 	    puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
 }
-
-
