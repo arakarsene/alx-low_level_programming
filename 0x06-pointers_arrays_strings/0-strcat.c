062f4edde29e2a2c0000c851fb0d06ef12ef7b40
arakarsene <arakarsebe@gmail.com>
2022-09-21 13:16:10 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char _strcat a functions that concatenates two strings 
+ * @desc: copy to 
+ * @src: copy from
+ * Return: two concat strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i,m;
+	for(i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for(m = 0; (dest[i+m] + *src++) != '\0'; m++)
+	{
+	}
+
+	return(dest);
+}
---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the caracter c to stdout
+ * @c : the caracter  to print
+ *
+ * Return: on success 1
+ * on error, -1 is returned, and error is set apropriately
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
