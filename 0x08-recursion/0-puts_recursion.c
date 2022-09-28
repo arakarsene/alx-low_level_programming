c46f9ad0b1c408c8b6fdca842dd147344d9d9d5a
arakarsene <arakarsebe@gmail.com>
2022-09-28 13:49:00 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\o')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(s[o]);
+		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
 }
